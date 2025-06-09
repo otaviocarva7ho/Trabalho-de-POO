7283c4c93751a69f443b43ffad831416d11f6be3
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-06-09 01:13:37 -0300

    Class ImovelMisto

--- ImovelMisto.java
+++ ImovelMisto.java
@@ -1,3 +1,17 @@
-public class ImovelMisto {
+public class ImovelMisto extends Imovel{
 
+    public ImovelMisto(int idImovel, String endereco, double valorBaseLocacao, double valorBaseVenda, double iptu) {
+        super(idImovel, endereco, valorBaseLocacao, valorBaseVenda, iptu);
+    }
+
+    @Override
+    public double calcularTaxaAdministracao(){
+        return getValorBaseLocacao() * 0.07;
+    }
+
+    @Override
+    public void mostraDados(){
+        super.mostraDados();
+        System.out.println("Taxa de Administração: R$" + calcularTaxaAdministracao());
+    }
 }
