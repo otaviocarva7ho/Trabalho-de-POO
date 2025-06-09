ededb80ac69fcf25716379ed557fc9d079c7652c
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-06-09 01:19:25 -0300

    Class ImovelIndustrial

---
+++ ImovelIndustrial.java
@@ -0,0 +1,17 @@
+public class ImovelIndustrial extends Imovel {
+
+    public ImovelIndustrial(int idImovel, String endereco, double valorBaseLocacao, double valorBaseVenda, double iptu) {
+        super(idImovel, endereco, valorBaseLocacao, valorBaseVenda, iptu);
+    }
+
+    @Override
+    public double calcularTaxaAdministracao() {
+        return getValorBaseLocacao() * 0.08;
+    }
+
+    @Override
+    public void mostraDados() {
+        super.mostraDados();
+        System.out.println("Taxa de Administração: R$" + calcularTaxaAdministracao());
+    }
+}
